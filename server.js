8f81bd5f40df1b3f0e5b95a9bb70912bf9d7328c
AmitJakharcodemyself <amit7665253807@gmail.com>
2021-02-16 21:17:58 +0530

    server port changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express();
+const port=process.env.PORT || 4444
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -9,4 +10,4 @@ app.use(express.urlencoded({extended: true}))
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/api', require('./routes/api').route)
 
-app.listen(2678, () => console.log('Server started at http://localhost:2678'))
+app.listen(port, () => console.log('Server started at http://localhost:2678'))
